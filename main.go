a3bc05b288d6cf5569d566f1ef18a98af29e9e5b
j-beckman <beckman.joshua@gmail.com>
2015-01-11 16:40:01 -0600

    Got the db figured out

--- main.go
+++ main.go
@@ -1,33 +1,66 @@
 package main
 
 import (
+    "os"
     "fmt"
-    "github.com/jbckmn/gopod"
     "time"
-    // "net/http"
+    "net/http"
+    "gopkg.in/mgo.v2"
+    "github.com/jbckmn/gopod"
+    "github.com/zenazn/goji"
+    "github.com/zenazn/goji/web"
 )
 
+var session = dbConnect()
+
 func main() {
-    c := gopod.ChannelFactory("Daniel's Channel", "http://RubyDeveloper.com/", "My Blog", "http://narro.co/images/narro-icon.png")
-    c.SetPubDate(time.Now().UTC())
-    c.SetiTunesExplicit("No")
 
-    c.AddItem(&gopod.Item{
-        Title:       "Ruby Developer",
-        Link:        "http://RubyDeveloper.com/",
-        Description: "Ruby Developer",
-        PubDate:     time.Now().UTC().Format(time.RFC1123),
-    })
+    goji.Get("/:vanity", renderPodcast)
+    goji.Serve()
 
-    // Example: Using an item's SetPubDate method
-    i := &gopod.Item{
-        Title:       "LinkedIn",
-        Link:        "http://www.linkedin.com/in/dangogh",
-        Description: "My LinkedIn",
+}
+
+func renderPodcast(c web.C, w http.ResponseWriter, r *http.Request) {
+    defer session.Close()
+    db := session.Clone().DB("audrey")
+    if db != nil {
+        fmt.Println("db session")
     }
-    i.SetPubDate(time.Now().Unix())
-    c.AddItem(i)
+    w.Header().Set("Content-Type", "text/plain") // for testing
+    // w.Header().Set("Content-Type", "application/rss+xml")
+    s := buildPodcast()
+    fmt.Fprintf(w, "%s", s.PublishIndent())
+}
 
-    fmt.Printf("%s\n\n", c.Publish())
-    fmt.Printf("%s\n\n", c.PublishIndent())
+func buildPodcast() *gopod.Channel {
+        c := gopod.ChannelFactory("Daniel's Channel", "http://RubyDeveloper.com/", "My Blog", "image.png")
+        c.SetPubDate(time.Now().UTC())
+
+        c.AddItem(&gopod.Item{
+                Title:       "Stack Overflow",
+                Link:        "http://stackoverflow.com/users/1305696/daniel",
+                Description: "Stack Overflow",
+                PubDate:     time.Now().UTC().Format(time.RFC1123),
+        })
+
+        i := &gopod.Item{
+                Title:       "LinkedIn",
+                Link:        "http://www.linkedin.com/in/dangogh",
+                Description: "My LinkedIn",
+        }
+        i.SetPubDate(time.Now().Unix())
+        c.AddItem(i)
+
+        return c
+        // return c.Publish()
+}
+
+func dbConnect() *mgo.Session {
+    u := os.Getenv("MONGO_URI")
+    session, err := mgo.Dial(u)
+    if err != nil {
+        fmt.Println("No db session")
+        panic(err)
+    }
+    return session
 }
